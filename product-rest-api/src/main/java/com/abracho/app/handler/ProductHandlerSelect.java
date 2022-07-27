b97e83dc62f0bc9aa14748b5dbe9a8ee4e8f11e3
4brach0 <67078286+4brach0@users.noreply.github.com>
2022-07-27 07:30:30 -0300

    refactor api

--- product-rest-api/src/main/java/com/abracho/app/handler/ProductHandlerSelect.java
+++ product-rest-api/src/main/java/com/abracho/app/handler/ProductHandlerSelect.java
@@ -1,14 +1,11 @@
 package com.abracho.app.handler;
 
-import org.omg.CORBA.BAD_INV_ORDER;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.reactive.function.server.ServerRequest;
