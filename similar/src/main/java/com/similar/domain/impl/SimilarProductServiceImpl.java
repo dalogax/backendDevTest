971d36d7faa3bf310e45b9bb5f8468a2bb0d851a
Jotxee <jotxee@jotxee.dev>
2022-12-05 16:18:01 +0100

    green test phase: invoke getSimilarProduct then return a List

--- similar/src/main/java/com/similar/domain/impl/SimilarProductServiceImpl.java
+++ similar/src/main/java/com/similar/domain/impl/SimilarProductServiceImpl.java
@@ -1,13 +1,16 @@
 package com.similar.domain.impl;
 
 import com.similar.domain.SimilarProductService;
+import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
+@Service
 public class SimilarProductServiceImpl implements SimilarProductService {
 
     @Override
     public List<String> getSimilarProductBy(Long productId) {
-        return null;
+        return Collections.emptyList();
     }
 }
