82bae265d9d01fa592bae028c2e24f95b09e6677
Jotxee <jotxee@jotxee.dev>
2022-12-05 16:10:01 +0100

    verify call to service in controller

--- similar/src/test/java/com/similar/application/SimilarProductControllerTest.java
+++ similar/src/test/java/com/similar/application/SimilarProductControllerTest.java
@@ -1,9 +1,11 @@
 package com.similar.application;
 
+import com.similar.domain.SimilarProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,7 +13,11 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
@@ -19,6 +25,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 class SimilarProductControllerTest {
 
     private MockMvc mvc;
+
+    @Mock
+    private SimilarProductService similarProductService;
 
     @InjectMocks
     private SimilarProductController sut;
@@ -31,6 +40,7 @@ class SimilarProductControllerTest {
     @Test
     public void returnsSimilarProductsById() throws Exception {
         // given
+        given(similarProductService.getSimilarProductBy(1L)).willReturn(Collections.emptyList());
 
         // when
         MockHttpServletResponse response = mvc.perform(
@@ -40,6 +50,7 @@ class SimilarProductControllerTest {
 
         // then
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        verify(similarProductService).getSimilarProductBy(1L);
     }
 
 
