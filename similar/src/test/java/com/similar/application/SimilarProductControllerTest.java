7d6fd720cf9b6fa3247e1514192c0c1dd977433f
Jotxee <jotxee@jotxee.dev>
2022-12-05 13:28:42 +0100

    red test phase: get 404 when call product/{producId}/similar

--- similar/src/test/java/com/similar/application/SimilarProductControllerTest.java
+++ similar/src/test/java/com/similar/application/SimilarProductControllerTest.java
@@ -1,7 +1,47 @@
 package com.similar.application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@ExtendWith(MockitoExtension.class)
 class SimilarProductControllerTest {
+
+    public static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json;charset=UTF-8";
+    private MockMvc mvc;
+
+    @InjectMocks
+    private SimilarProductController sut;
+
+    @BeforeEach
+    public void setup(){
+        mvc = MockMvcBuilders.standaloneSetup(sut).build();
+    }
+
+    @Test
+    public void returnsSimilarProductsById() throws Exception {
+        // given
+
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                        get("/product/1/similar")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
 
 }
