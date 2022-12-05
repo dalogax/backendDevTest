76833af0a62df780ec3880a9ac7e2c5dfdca94e0
Jotxee <jotxee@jotxee.dev>
2022-12-05 12:33:07 +0100

    first commit - add similar project by Spring Initializr

---
+++ similar/src/main/java/com/similar/SimilarProductsApplication.java
@@ -0,0 +1,13 @@
+package com.similar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductsApplication.class, args);
+	}
+
+}
