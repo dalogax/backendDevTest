2d27e43ac8ed29bf86a3c1a9cb26aa11b27fe5a2
Jotxee <jotxee@jotxee.dev>
2022-12-05 15:40:42 +0100

    create the implementation of similar-product service

---
+++ similar/src/main/java/com/similar/domain/impl/SimilarProductServiceImpl.java
@@ -0,0 +1,13 @@
+package com.similar.domain.impl;
+
+import com.similar.domain.SimilarProductService;
+
+import java.util.List;
+
+public class SimilarProductServiceImpl implements SimilarProductService {
+
+    @Override
+    public List<String> getSimilarProductBy(Long productId) {
+        return null;
+    }
+}
