fb404d7df54f93115af88475a6fdaa49614ce1ce
Jotxee <jotxee@jotxee.dev>
2022-12-05 12:41:42 +0100

    add first test to check that the application.

--- similar/src/test/java/com/similar/SimilarProductsApplicationTests.java
+++ similar/src/test/java/com/similar/SimilarProductsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.similar;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SimilarProductsApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext);
 	}
 
 }
