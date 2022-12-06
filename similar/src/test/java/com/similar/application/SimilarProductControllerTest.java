957bd4625c22021864688543bdc6be1c46fb5031
Jotxee <jotxee@jotxee.dev>
2022-12-06 20:14:12 +0100

    add test for check the body with a correct response

--- similar/src/test/java/com/similar/application/SimilarProductControllerTest.java
+++ similar/src/test/java/com/similar/application/SimilarProductControllerTest.java
@@ -1,19 +1,25 @@
 package com.similar.application;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.similar.application.exception.SimulatedClientExceptionHandler;
 import com.similar.domain.SimilarProductService;
+import com.similar.domain.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -25,6 +31,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 class SimilarProductControllerTest {
 
     private MockMvc mvc;
+    private JacksonTester<List<Product>> jsonSimilarProducts;
 
     @Mock
     private SimilarProductService similarProductService;
@@ -34,7 +41,8 @@ class SimilarProductControllerTest {
 
     @BeforeEach
     public void setup(){
-        mvc = MockMvcBuilders.standaloneSetup(sut).build();
+        mvc = MockMvcBuilders.standaloneSetup(sut).setControllerAdvice(new SimulatedClientExceptionHandler()).build();
+        JacksonTester.initFields(this, new ObjectMapper());
     }
 
     @Test
@@ -51,6 +59,33 @@ class SimilarProductControllerTest {
         // then
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
         verify(similarProductService).getSimilarProductBy(1L);
+    }
+
+    @Test
+    public void should_return_similar_products() throws Exception {
+        // given
+        given(similarProductService.getSimilarProductBy(1L)).willReturn(getMockSimilarProducts());
+
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                        get("/product/1/similar")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        verify(similarProductService).getSimilarProductBy(1L);
+        assertThat(response.getContentAsString()).isEqualTo(
+                jsonSimilarProducts.write(getMockSimilarProducts()).getJson()
+        );
+
+    }
+
+    private List<Product> getMockSimilarProducts() {
+        return List.of(
+                new Product( "2","Dress", BigDecimal.valueOf(19.99), true),
+                new Product( "4","Boots", BigDecimal.valueOf(39.99), true)
+        );
     }
 
 
