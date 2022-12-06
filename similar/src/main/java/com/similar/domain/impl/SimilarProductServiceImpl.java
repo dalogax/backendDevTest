be28b8c9a1e241ce3626689a5c85fd1c36fa7192
Jotxee <jotxee@jotxee.dev>
2022-12-06 17:00:18 +0100

    green test phase: call SimulatedClient but verify is invoke

--- similar/src/main/java/com/similar/domain/impl/SimilarProductServiceImpl.java
+++ similar/src/main/java/com/similar/domain/impl/SimilarProductServiceImpl.java
@@ -1,16 +1,30 @@
 package com.similar.domain.impl;
 
 import com.similar.domain.SimilarProductService;
+import com.similar.infrastructure.client.SimulatedClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class SimilarProductServiceImpl implements SimilarProductService {
 
+    SimulatedClient client;
+
     @Override
     public List<String> getSimilarProductBy(Long productId) {
+        this.client.getSimilar(getHeaders(), productId);
         return Collections.emptyList();
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
     }
 }
--- similar/src/main/java/com/similar/infrastructure/client/SimulatedClient.java
+++ similar/src/main/java/com/similar/infrastructure/client/SimulatedClient.java
@@ -1,11 +1,11 @@
 package com.similar.infrastructure.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import java.net.http.HttpHeaders;
 import java.util.List;
 
 @FeignClient
