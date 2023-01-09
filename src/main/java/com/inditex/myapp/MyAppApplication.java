35d7b0b0c99cb1869d19115d099261148fc6b9dc
TMRGZ <miguelrg5605@outlook.es>
2023-01-09 20:49:36 +0100

    First commit

---
+++ src/main/java/com/inditex/myapp/MyAppApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inditex/myapp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
