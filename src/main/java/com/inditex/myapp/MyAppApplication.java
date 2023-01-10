77941b1f6a11fb144b60fb8f9f68daa25dc22fac
TMRGZ <miguelrg5605@outlook.es>
2023-01-10 23:34:12 +0100

    FEATURE: Enabled config properties

--- src/main/java/com/inditex/myapp/MyAppApplication.java
+++ src/main/java/com/inditex/myapp/MyAppApplication.java
@@ -2,8 +2,10 @@ package com.inditex.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class MyAppApplication {
 
     public static void main(String[] args) {
