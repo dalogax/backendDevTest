024519a6afbcbdb27fae60209d2824c23336a4b2
TMRGZ <miguelrg5605@outlook.es>
2023-01-11 23:23:53 +0100

    FEATURE: Implemented product app service

--- src/main/java/com/inditex/myapp/application/service/impl/ProductApplicationServiceImpl.java
+++ src/main/java/com/inditex/myapp/application/service/impl/ProductApplicationServiceImpl.java
@@ -1,12 +1,15 @@
 package com.inditex.myapp.application.service.impl;
 
+import com.inditex.myapp.application.mapper.OutputProductDetailMapper;
 import com.inditex.myapp.application.service.ProductApplicationService;
+import com.inditex.myapp.domain.model.ProductDetail;
 import com.inditex.myapp.domain.service.ProductService;
 import com.inditex.myapp.infrastructure.controller.model.ProductDetailDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -15,10 +18,12 @@ public class ProductApplicationServiceImpl implements ProductApplicationService 
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private OutputProductDetailMapper outputProductDetailMapper;
+
     @Override
     public ResponseEntity<Set<ProductDetailDto>> getProductSimilar(String productId) {
-        productService.productSimilar(productId);
-
-        return null;
+        List<ProductDetail> similarProductDetailList = productService.productSimilar(productId);
+        return ResponseEntity.ok(outputProductDetailMapper.map(similarProductDetailList));
     }
 }
