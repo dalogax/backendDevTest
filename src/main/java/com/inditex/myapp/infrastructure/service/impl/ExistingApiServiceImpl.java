71234cb83dd78139a0abb5f130917bd68ffa911e
TMRGZ <miguelrg5605@outlook.es>
2023-01-11 23:40:25 +0100

    FEATURE: Implemented existing api service

--- src/main/java/com/inditex/myapp/infrastructure/service/impl/ExistingApiServiceImpl.java
+++ src/main/java/com/inditex/myapp/infrastructure/service/impl/ExistingApiServiceImpl.java
@@ -1,13 +1,16 @@
 package com.inditex.myapp.infrastructure.service.impl;
 
+import com.inditex.myapp.application.model.ProductDetailResponse;
 import com.inditex.myapp.domain.model.ProductDetail;
 import com.inditex.myapp.domain.service.ExistingApiService;
 
+import com.inditex.myapp.infrastructure.mapper.InputProductDetailMapper;
 import com.inditex.myapp.infrastructure.rest.DefaultApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ExistingApiServiceImpl implements ExistingApiService {
@@ -15,18 +18,18 @@ public class ExistingApiServiceImpl implements ExistingApiService {
     @Autowired
     private DefaultApi defaultApi;
 
+    @Autowired
+    private InputProductDetailMapper inputProductDetailMapper;
+
     @Override
     public ProductDetail getProduct(String productId) {
-
-
-
-        defaultApi.getProductProductId(productId);
-        return null;
+        ProductDetailResponse productDetailResponse = defaultApi.getProductProductId(productId);
+        return inputProductDetailMapper.map(productDetailResponse);
     }
 
     @Override
     public List<String> getSimilarProducts(String productId) {
-        defaultApi.getProductSimilarids(productId);
-        return null;
+        Set<String> productSimilaridSet = defaultApi.getProductSimilarids(productId);
+        return productSimilaridSet.stream().toList();
     }
 }
