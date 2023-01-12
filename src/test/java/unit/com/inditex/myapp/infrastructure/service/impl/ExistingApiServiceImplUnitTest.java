1b682f23d6183d5a18313a9826ca60cdacea8b6b
TMRGZ <miguelrg5605@outlook.es>
2023-01-12 23:52:26 +0100

    FEATURE: Added missing unit tests for exceptions

--- src/test/java/unit/com/inditex/myapp/infrastructure/service/impl/ExistingApiServiceImplUnitTest.java
+++ src/test/java/unit/com/inditex/myapp/infrastructure/service/impl/ExistingApiServiceImplUnitTest.java
@@ -1,6 +1,8 @@
 package unit.com.inditex.myapp.infrastructure.service.impl;
 
 import com.inditex.myapp.application.model.ProductDetailResponse;
+import com.inditex.myapp.infrastructure.exception.ExistingApisErrorException;
+import com.inditex.myapp.infrastructure.exception.ProductNotFoundException;
 import com.inditex.myapp.infrastructure.mapper.InputProductDetailMapper;
 import com.inditex.myapp.infrastructure.rest.DefaultApi;
 import com.inditex.myapp.infrastructure.service.impl.ExistingApiServiceImpl;
@@ -11,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.Collections;
 import java.util.List;
@@ -38,6 +42,18 @@ class ExistingApiServiceImplUnitTest {
     }
 
     @Test
+    void getProductButClientErrorExceptionUnitTest() {
+        Mockito.when(defaultApi.getProductProductId(Mockito.anyString())).thenThrow(HttpClientErrorException.class);
+        Assertions.assertThrows(ProductNotFoundException.class, () -> existingApiService.getProduct("TEST"));
+    }
+
+    @Test
+    void getProductButServerErrorExceptionUnitTest() {
+        Mockito.when(defaultApi.getProductProductId(Mockito.anyString())).thenThrow(HttpServerErrorException.class);
+        Assertions.assertThrows(ExistingApisErrorException.class, () -> existingApiService.getProduct("TEST"));
+    }
+
+    @Test
     void getSimilarProductsUnitTest() {
         Mockito.when(defaultApi.getProductSimilarids(Mockito.anyString())).thenReturn(Collections.emptySet());
 
@@ -45,5 +61,17 @@ class ExistingApiServiceImplUnitTest {
 
         Assertions.assertNotNull(similarProducts);
         Mockito.verify(defaultApi).getProductSimilarids(Mockito.any());
+    }
+
+    @Test
+    void getSimilarProductsButClientErrorExceptionUnitTest() {
+        Mockito.when(defaultApi.getProductSimilarids(Mockito.anyString())).thenThrow(HttpClientErrorException.class);
+        Assertions.assertThrows(ProductNotFoundException.class, () -> existingApiService.getSimilarProducts("TEST"));
+    }
+
+    @Test
+    void getSimilarProductsButServerErrorExceptionUnitTest() {
+        Mockito.when(defaultApi.getProductSimilarids(Mockito.anyString())).thenThrow(HttpServerErrorException.class);
+        Assertions.assertThrows(ExistingApisErrorException.class, () -> existingApiService.getSimilarProducts("TEST"));
     }
 }
