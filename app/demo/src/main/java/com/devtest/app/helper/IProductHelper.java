09a125d35340c72da49fd5df341ae7d0562f5a16
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-02-13 19:39:33 +0100

    update product helper

--- app/demo/src/main/java/com/devtest/app/helper/IProductHelper.java
+++ app/demo/src/main/java/com/devtest/app/helper/IProductHelper.java
@@ -1,5 +1,9 @@
 package com.devtest.app.helper;
 
+import com.devtest.app.entities.Product;
+import com.devtest.app.exceptions.GenericException;
+
 public interface IProductHelper {
-    
+    public String[] getSimilarProductsIds(String id) throws GenericException;
+    public Product getProductById(String id) throws GenericException;
 }
--- app/demo/src/main/java/com/devtest/app/helper/impl/ProductHelper.java
+++ app/demo/src/main/java/com/devtest/app/helper/impl/ProductHelper.java
@@ -1,34 +1,36 @@
 package com.devtest.app.helper.impl;
 
 import com.devtest.app.entities.Product;
+import com.devtest.app.exceptions.GenericException;
 import com.devtest.app.helper.IProductHelper;
+import com.devtest.app.utils.Constants;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
-
+@Repository
 public class ProductHelper implements IProductHelper{
-    private final String URL = "http://localhost:3001/product/";
-    private final String PATH = "/similarids";
-
-
-    public String[] getSimilarProductsIds(String productId) {
+    public String[] getSimilarProductsIds(String id) throws GenericException {
         RestTemplate restTemplate = new RestTemplate();
         try{
             ResponseEntity<String[]> response =
-                    restTemplate.getForEntity(URL + productId + PATH,
+                    restTemplate.getForEntity(Constants.HOST_MOCKS + Constants.PRODUCT + "/" + id + Constants.PRODUCT_SIMILARS_ID,
                             String[].class);
             return response.getBody();
-        } catch (Exception e){
-            return null;
+        } 
+        catch (Exception e){
+            throw new GenericException("An error occurred while obtaining the similar products on mocks");
         }
     }
 
-    public Product getProductById(String productId) {
+    public Product getProductById(String id) throws GenericException {
         RestTemplate restTemplate = new RestTemplate();
         try{
-            ResponseEntity<Product> response = restTemplate.getForEntity(URL + productId, Product.class);
+            ResponseEntity<Product> response = restTemplate.getForEntity(Constants.HOST_MOCKS + Constants.PRODUCT + "/" + id, Product.class);
             return response.getBody();
-        } catch (Exception e){
-            return null;
+        } 
+        catch (Exception e){
+            throw new GenericException("An error occurred while obtaining product by id on mocks");
         }
     }
 }
