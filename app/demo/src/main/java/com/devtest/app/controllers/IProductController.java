1aba3596d96117192f5d6a898982ed256457a347
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-02-13 22:32:16 +0100

    add java docs

--- app/demo/src/main/java/com/devtest/app/controllers/IProductController.java
+++ app/demo/src/main/java/com/devtest/app/controllers/IProductController.java
@@ -9,5 +9,13 @@ import com.devtest.app.entities.Product;
 import com.devtest.app.exceptions.GenericException;
 
 public interface IProductController {
+    /**
+    * Controller that gets the similar products from an id
+    *
+    * @param id  id where we get from the similar products
+    * @return List<Product> list of similiar products
+    * @throws GenericException  If an input has no valid value or missing 
+    *                    
+    */
     public ResponseEntity<List<Product>> getSimilarProductsById(@PathVariable("productId") String id) throws GenericException;
 }
--- app/demo/src/main/java/com/devtest/app/services/IProductService.java
+++ app/demo/src/main/java/com/devtest/app/services/IProductService.java
@@ -6,5 +6,13 @@ import com.devtest.app.entities.Product;
 import com.devtest.app.exceptions.GenericException;
 
 public interface IProductService {
+    /**
+    * Service that gets the similar products from a an id
+    *
+    * @param id  id where we get from the similar products
+    * @return List<Product> list of similiar products
+    * @throws GenericException  If an input has no valid value or missing 
+    *                    
+    */
     List<Product> getSimilarProductsById(String productId) throws GenericException;
 }
