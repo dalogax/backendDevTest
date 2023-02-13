d2409b6f6a92e768fc2d4514a68425e66c545d93
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-02-13 19:39:43 +0100

    update product service

--- app/demo/src/main/java/com/devtest/app/services/impl/ProductService.java
+++ app/demo/src/main/java/com/devtest/app/services/impl/ProductService.java
@@ -2,25 +2,20 @@ package com.devtest.app.services.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devtest.app.entities.Product;
 import com.devtest.app.exceptions.GenericException;
-import com.devtest.app.helper.IProductHelper;
 import com.devtest.app.helper.impl.ProductHelper;
 import com.devtest.app.services.IProductService;
-import com.devtest.app.utils.Constants;
-import com.devtest.app.utils.Utils;
 
 @Service
 public class ProductService implements IProductService {
 
 	@Autowired
-    private ProductHelper productsClient;
+    private ProductHelper productHelper;
 	
 	public List<Product> getSimilarProductsById(String id) throws GenericException{
 		if(id == null) throw new GenericException("Product " + id + " not found");
@@ -28,25 +23,17 @@ public class ProductService implements IProductService {
 		List<Product> products = new ArrayList<>();
 		
 		try {
-			RestTemplate restTemplate = new RestTemplate();
-			//TODO: refacto castLIst
-			List<Integer> similarids = Utils.castList(Integer.class, restTemplate.getForObject(Constants.GET_PRODUCT_SIMILAR_IDS.replace("{productId}", id), ArrayList.class));
-			
-			products = similarids
-					.stream()
-					.map(i -> restTemplate.getForObject(Constants.GET_PRODUCT_DETAIL.replace("{productId}", i.toString()), Product.class))
-					.collect(Collectors.toList());
-			
-			String[] similarProductsIds =  productsClient.getSimilarProductsIds(productId);
-			if( similarProductsIds == null) return null;
+			String[] similarProductsIds = productHelper.getSimilarProductsIds(id);
+			if(similarProductsIds == null) throw new GenericException("Not similar products found");
 
-			for (String id : similarProductsIds) {
-			Product product = productsClient.getProductById(id);
-			if( product != null) {
-				products.add(product);
-			}
+			for (String similarId : similarProductsIds) {
+				Product product = productHelper.getProductById(similarId);
+				if (product != null) {
+					products.add(product);
+				}
 			}	
 		} 
+
 		//TODO: manage better exceptions
 		catch (Exception e) {
 			throw new GenericException("An error occurred while obtaining the similar products");
@@ -54,5 +41,4 @@ public class ProductService implements IProductService {
 		
 		return products;
 	}
-	
 }
