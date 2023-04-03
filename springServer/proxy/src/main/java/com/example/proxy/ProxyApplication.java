94189cd9b8e95c691831991c3fd0cbd1624408e0
mariano <mbrayotta@gmail.com>
2023-04-02 22:25:58 -0300

    init

---
+++ springServer/proxy/src/main/java/com/example/proxy/ProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+}
