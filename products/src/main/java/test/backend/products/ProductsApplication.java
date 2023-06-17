6570ac56c207276f8e0ead46b8ad34a0a444d2ad
jFompe <jaime.fompe@gmail.com>
2023-06-17 10:34:25 +0200

    Products API initial state

---
+++ products/src/main/java/test/backend/products/ProductsApplication.java
@@ -0,0 +1,13 @@
+package test.backend.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApplication.class, args);
+	}
+
+}
