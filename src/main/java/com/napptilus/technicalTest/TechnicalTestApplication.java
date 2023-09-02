e997e9cbc84287eb118c4382a8510636010230a7
Ángel Pérez Llorente <angel.perezl@alumnos.upm.es>
2023-09-02 19:39:05 +0200

    Project structure

---
+++ src/main/java/com/napptilus/technicalTest/TechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.napptilus.technicalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/napptilus/technicalTest/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napptilus.technicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
