554278c12a059134be495ef4db48a96254b27cc5
Nauzet Lopez Mendoza <nauzetlm90@gmail.com>
2025-04-14 14:02:44 +0100

    feat: add initial Spring Boot application and configuration
    This commit introduces the initial setup for the Spring Boot application, including the main application class `SimilarProductsApplication` and the `application.properties` file with server port and external API URL configurations.

---
+++ src/main/java/com/example/backendDevTest/SimilarProductsApplication.java
@@ -0,0 +1,12 @@
+package com.example.backendDevTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimilarProductsApplication.class, args);
+    }
+}
