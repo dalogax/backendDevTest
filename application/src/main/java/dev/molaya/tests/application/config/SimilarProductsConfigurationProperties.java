4bc8b917fe567052913f4b824306d283821090be
molaya <me@molaya.dev>
2025-11-28 14:34:49 +0100

    feat(configuration): improve configuration properties

--- application/src/main/java/dev/molaya/tests/application/config/SimilarProductsConfigurationProperties.java
+++ application/src/main/java/dev/molaya/tests/application/config/SimilarProductsConfigurationProperties.java
@@ -2,9 +2,7 @@ package dev.molaya.tests.application.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "config.similar-products")
 public record SimilarProductsConfigurationProperties(
         @DefaultValue("20") int limit, @DefaultValue("20") int parallelRequests) {}
--- bootstrap/src/main/java/dev/molaya/tests/BackProductsApplication.java
+++ bootstrap/src/main/java/dev/molaya/tests/BackProductsApplication.java
@@ -1,9 +1,12 @@
 package dev.molaya.tests;
 
+import dev.molaya.tests.application.config.SimilarProductsConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SimilarProductsConfigurationProperties.class)
 public class BackProductsApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackProductsApplication.class, args);
