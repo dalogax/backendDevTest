d9a0bf8b5213cd4f6f85922455c9d6938b17ee58
Ineiles Reyes <ineiles.reyes@gmail.com>
2026-04-22 12:46:39 +0200

    First commit. Add base project.

---
+++ products/src/test/java/org/challenge/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.challenge.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
