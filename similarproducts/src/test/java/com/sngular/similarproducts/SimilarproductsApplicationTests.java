415ef173d5f7c211a308e37ab1e87d6c76891b22
Fran Graciani <graciani@gmail.com>
2026-05-10 16:44:01 +0200

    feat: add initial project structure and configuration files for Similar Products service

---
+++ similarproducts/src/test/java/com/sngular/similarproducts/SimilarproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sngular.similarproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
